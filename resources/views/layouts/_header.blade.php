ae504cb733d71d5b1c430884e8f0c60184d59260
vagrant <vagrant@homestead>
2018-09-20 15:05:12 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,14 @@
+<header id="header" class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{route('home')}}" title="logo" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ route('help') }}" title="帮助">帮助</a></li>
+                    <li><a href="{{route('about')}}" title="关于">关于</a></li>
+                    <li><a href="#" title="登录">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    </header><!-- /header -->
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>@yield('title','Sample App') - Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
 </body>
 </html>
